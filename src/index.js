593099cc726d4f9e9a8f22f64860711dea58769c
MarketaZ <marketa.zadrazilova@concur.com>
2019-03-21 21:27:40 +0100

    Hot module replacement added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const renderApp = () => ReactDOM.render(<App />, document.getElementById('root'));
+
+// hot module replacement (hmr)
+if (module.hot) {
+    module.hot.accept('./App.js', renderApp);
+}
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
