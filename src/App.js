a2395954796331377ab6fd5cb1f7867201003c4c
unknown <m.zadrazilova@gmail.com>
2019-04-28 10:00:12 +0200

    Error boundary added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { GlobalStyles } from './globalStyles'
 import store from './store'
 import * as routes from './routes'
 
+import { ErrorBoundary } from './components/ErrorBoundary'
 import { Layout } from './components/Layout'
 import { PrivateRoute } from './components/PrivateRoute'
 import { Products } from './pages/Products'
@@ -26,22 +27,24 @@ const App = () => (
   <Provider store={store}>
     <Router>
       <GlobalStyles />
-      <Layout>
-        <Switch>
-          <Route
-            path={routes.HOMEPAGE}
-            exact
-            render={() => <Redirect to={routes.PRODUCT_LIST} />}
-          />
-          <Route path={routes.PRODUCT_LIST} exact component={Products} />
-          <Route path={routes.PRODUCT_DETAIL} component={ProductDetail} />
-          <Route path={routes.CART} component={Cart} />
-          <Route path={routes.SIGN_UP} component={SignUp} />
-          <Route path={routes.LOGIN} component={Login} />
-          <PrivateRoute path={routes.MY_ACCOUNT} component={MyAccount} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
+      <ErrorBoundary>
+        <Layout>
+          <Switch>
+            <Route
+              path={routes.HOMEPAGE}
+              exact
+              render={() => <Redirect to={routes.PRODUCT_LIST} />}
+            />
+            <Route path={routes.PRODUCT_LIST} exact component={Products} />
+            <Route path={routes.PRODUCT_DETAIL} component={ProductDetail} />
+            <Route path={routes.CART} component={Cart} />
+            <Route path={routes.SIGN_UP} component={SignUp} />
+            <Route path={routes.LOGIN} component={Login} />
+            <PrivateRoute path={routes.MY_ACCOUNT} component={MyAccount} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </ErrorBoundary>
     </Router>
   </Provider>
 )
